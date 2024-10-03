c00f1b665348a01aadb1dd0b50a2b563ee3383f1
imyhalex <ys4689@nyu.edu>
2024-10-03 19:26:12 -0400

    db initial setup

--- utils/exercise_db.py
+++ utils/exercise_db.py
@@ -1,11 +1,18 @@
 # database implementation
+import os
 import pymongo
 from dotenv import load_dotenv
 
+# load environment variables from .env file (this is not included in version control)
 load_dotenv()
 
-myclient = pymongo.MongoClient("MONGO_URI")
+# fetch the MONGO_URI from environmental variables
+mongo_uri = os.getenv("MONGO_URI")
 
+# initialized mongodb client with fetched uri
+myclient = pymongo.MongoClient(mongo_uri)
+
+# access the 'exercise_db' database
 exercise_db = myclient["exercise_db"]
 
 print(myclient.list_database_names())
