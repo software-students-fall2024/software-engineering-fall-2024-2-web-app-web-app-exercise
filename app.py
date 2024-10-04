2bb4964eaec4eda843223106b84b0346ee06d8fd
imyhalex <ys4689@nyu.edu>
2024-10-04 12:37:15 -0400

    update

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect, abort, url_for, make_response
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from flask import Flask, render_template, request, redirect, abort, url_for, make_response, flash
 
 
 load_dotenv()
 
 app = Flask(__name__)
 
+# index means home
+@app.route("/")
+def index():
+    return ""
+
+
+# app.run(host="0.0.0.0", port=80)
