bd9b605616bcfb510ace83a7a3cf3083e067399a
Simon Sherbet <sas9889@nyu.edu>
2024-10-07 13:32:07 -0400

    majoring refactoring, basic login/ sign up

--- src/app.py
+++ src/app.py
@@ -3,24 +3,25 @@ import datetime
 from flask import Flask, render_template, request, redirect, url_for
 import pymongo
 from dotenv import load_dotenv
+import flask
+from src.database_initialisation import *
+import flask_login
+from flask_login import LoginManager
+from dotenv import dotenv_values
 
-from database_initialisation import *
-
-
+config = dotenv_values(".env")
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client['db']
+database_add(db, "pedals", "src/data/pedal_data.json")
+database_add(db, "users", "src/data/user_data.json")
+login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
-
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client['db']
-    database_add(db, "pedals", "data/pedal_data.json")
-
-    @app.route("/")
-    def login():
-        return render_template('login.html')
-
+    app.secret_key = config['SECRET_KEY']
+    login_manager.init_app(app)
+    register_blueprint(app)
     return app
 
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(port='5000')
+def register_blueprint(app):
+    from src.routes import routes
+    app.register_blueprint(routes)
