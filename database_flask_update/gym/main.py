4dc0d70d1068e8bc6472b8f5b305752d6a391e51
AndreaTang123 <xt2073@nyu.edu>
2024-10-07 20:22:19 -0400

    as

--- database_flask_update/gym/main.py
+++ database_flask_update/gym/main.py
@@ -1,14 +1,39 @@
 from flask import Blueprint, render_template
 from .extensions import mongo
+from flask import request
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     user_collection = mongo.db.workoutInstructions
-    document = user_collection.find_one({"id": "3303"})
-    if document:
+    ids_to_find = ["3303","0112","0634","1330","0199","0746","0191","1308","1263"]
+    documents = user_collection.find({"id": {"$in":  ids_to_find}})
+    workouts=[]
+    for document in documents:
         path = document.get("gif_path")
-        return render_template('index.html', gif_path=path)
-    else:
-        return "Document with id 3303 not found"
+        categories = document.get("categories")
+        id = document.get("id")
+        name = document.get("name")
+        instructions = document.get("instructions")
+        target_muscle = document.get("target_muscle")
+        workouts.append({"gif_path": path, "categories": categories, "id": id, "name": name, "instructions": instructions, "target_muscle": target_muscle})  # Append each workout to the list
+
+    return render_template('workout_instruction.html', workouts=workouts)  
+
+@main.route('/filter_workouts', methods=['POST'])
+def filter_workouts():
+    selected_category = request.form.get('category')  # Get the selected category from the form
+    user_collection = mongo.db.workoutInstructions
+    documents = user_collection.find({"categories": selected_category})  # Filter by category
+    workouts = []
+    for document in documents:
+        path = document.get("gif_path")
+        categories = document.get("categories")
+        id = document.get("id")
+        name = document.get("name")
+        instructions = document.get("instructions")
+        target_muscle = document.get("target_muscle")
+        workouts.append({"gif_path": path, "categories": categories, "id": id, "name": name, "instructions": instructions, "target_muscle": target_muscle})
+
+    return render_template('workout_instruction.html', workouts=workouts, selected_category=selected_category)
