ce9683e06f8fa4c61db3b528f70cd19d46fe2e25
StephenS2021 <sspencerwong28@gmail.com>
2024-10-09 15:07:34 -0400

    Added basic request page

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from pymongo import MongoClient, server_api
 app = Flask(__name__, static_url_path="", static_folder="static", template_folder="templates")
 
 reports = ["Vending machine", "Water fountain", "Door hinge"]
@@ -12,5 +12,11 @@ def index():
 def list():
     return render_template("list.html", reports=reports)
 
+@app.route("/request")
+@app.route("/request/<requestID>")
+def makeRequest(requestID=None):
+    return render_template("request.html", requestID=requestID)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run( host="127.0.0.1", port=3000)
