2ba2171bc95214546423a8530c91455723f65f5d
Simon Sherbet <sas9889@nyu.edu>
2024-10-09 18:57:20 -0400

    profile now loads user created boards

--- src/routes/profile.py
+++ src/routes/profile.py
@@ -6,7 +6,10 @@ import flask
 import flask_login
 from src.models.user import user_loader, request_loader
 from src.models.user import User
+from src.models.boards import get_boards_by_user
 
-@routes.route('/profile', methods=["GET"])
+@routes.route('/profile', methods=["GET", "POST"])
 def home():
-    return(render_template('profile.html'))
+    boards = get_boards_by_user(flask_login.current_user.email)
+    boards = boards.to_list()
+    return(render_template('profile.html', username=flask_login.current_user.username, boards=boards))
