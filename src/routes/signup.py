0d30a55a4f879f0925d915009c4bac827133525a
Simon Sherbet <sas9889@nyu.edu>
2024-10-09 19:03:25 -0400

    added logout

--- src/routes/signup.py
+++ src/routes/signup.py
@@ -12,7 +12,6 @@ from src.models.user import user_loader, request_loader, User, find_user, create
 @routes.route("/sign-up", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
-        # Validate form data
         username = request.form.get("username")
         password = request.form.get("password")
         email = request.form.get("email")
@@ -20,15 +19,11 @@ def signup():
         if not (username and password and email):
             return render_template("sign-up.html", message="All fields are required.")
 
-        # Hash the password
-        users = get_db().users
-
         if find_user(email):
             return render_template("sign-up.html", message="This email is already associated with an account!")
         
         create_user(email, username, password)
-        # Store user data in the database
-        # Your database insertion code goes here
+
 
         return redirect("login")
 
