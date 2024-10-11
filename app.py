be39ba0cad2026452cb48e352774fc385db0bb48
Jessica <kx2053@nyu.edu>
2024-10-11 19:03:40 -0400

    Added Flask-Login functionality for user authentication

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
 import os
-import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash
-import pymongo
-from bson.objectid import ObjectId
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
+class User(UserMixin):
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+
 def create_app():
-    """
-    Create and configure the Flask application.
-    returns: app: the Flask application object
-    """
     app = Flask(__name__)
-    app.secret_key = os.getenv('SECRET_KEY') 
+    app.secret_key = os.getenv('SECRET_KEY')
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'login'
+    login_manager.init_app(app)
+
+    client = MongoClient(os.getenv('MONGO_URI'))
+    db = client.get_database("your_database_name")
+    users_collection = db["users"]
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        user_data = users_collection.find_one({"_id": ObjectId(user_id)})
+        if user_data:
+            return User(str(user_data['_id']), user_data['username'])
+        return None
 
     @app.route("/main")
+    @login_required
     def home():
         programs = [{"p_name": "1"}, {"p_name": "2"}]
         return render_template("main.html", programs=programs)
@@ -26,16 +42,28 @@ def create_app():
             username = request.form['username']
             password = request.form['password']
 
-            if username == "admin" and password == "password123":
+        
+            user_data = users_collection.find_one({"username": username})
+            if user_data and user_data['password'] == password:  
+                user_obj = User(str(user_data['_id']), user_data['username'])
+                login_user(user_obj)
                 flash("Login successful!", "success")
                 return redirect(url_for('home'))
             else:
                 flash("Invalid credentials, please try again.", "danger")
         
         return render_template("login.html")
-    
+
+    @app.route("/logout")
+    @login_required
+    def logout():
+        logout_user()
+        flash("Logged out successfully.", "info")
+        return redirect(url_for('login'))
+
     return app 
 
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
+
