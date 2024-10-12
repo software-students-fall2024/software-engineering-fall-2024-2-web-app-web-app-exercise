b6e9262792da0224ac6cb7aada7b512aaebb9a6b
ziqiu-wang <2236531605@qq.com>
2024-10-11 22:35:32 -0400

    added mongodb import

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ from flask import Flask, render_template, request, redirect, url_for
 import pymongo
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
