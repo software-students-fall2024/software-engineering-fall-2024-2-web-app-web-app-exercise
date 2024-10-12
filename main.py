738a71b531a18757f336a6fac982f59c7087999b
StephenS2021 <sspencerwong28@gmail.com>
2024-10-12 17:15:37 -0400

    Added support for looking up appliance codes in DB

--- main.py
+++ main.py
@@ -8,24 +8,27 @@ import os
 import re
 
 # set up mongodb database connection
-mongo_host = os.getenv("MONGO_HOSt")
+mongo_host = os.getenv("MONGO_HOST")
 db_name = os.getenv("MONGO_DBNAME")
 
 client = MongoClient(mongo_host, server_api=server_api.ServerApi('1'))
-
+client.admin.command('ping')
 db = client[db_name]
 requests_collection = db.requests
+appliance_collection = db.appliances
+
+appliance_collection.insert_one({'code': 1234, 'building':"CIWW", 'floor':2, 'applianceName':'Water Fountain'})
 
 reports = ["Vending machine", "Water fountain", "Door hinge"]
 fakeCodes = [{'code': 1234, 'building':"CIWW", 'floor':2, 'applianceName':'Water Fountain'}]
 
 # Request logging
-# @app.before_request
-# def log_request_info():
-#     print("Request Method:", request.method)
-#     print("Request URL:", request.url)
-#     print("Request Headers:", request.headers)
-#     print("Request Data:", request.get_data())
+@app.before_request
+def log_request_info():
+    print("Request Method:", request.method)
+    print("Request URL:", request.url)
+    print("Request Headers:", request.headers)
+    print("Request Data:", request.get_data())
 
 @app.route("/")
 def index():
@@ -38,16 +41,17 @@ def list():
 @app.route("/request", methods=["GET", "POST"])
 def makeRequest(code=None):
     entry = None
-    if(request.method == 'POST'):
+    if(request.method == 'GET'):
         # If code is not empty and is 4 numbers
-        if(code := request.form.get('code')) != '' and re.match(r'^[0-9]{4}$', code):
+        if ((code := request.args.get('code')) != '' and code is not None and re.match(r'^[0-9]{4}$', code)):
             code = int(code)
             # If code exists, retrieve data as entry and display it 
-            # NOTE: This will be replaced by an actual request to our database for the appliance info
-            entry = next((entry for entry in fakeCodes if entry["code"] == code), None)
+            entry = appliance_collection.find_one({'code': code})
         else:
             entry = None
-            # Probably display something like code not found
+    if(request.method == 'POST'):
+        # If code is not empty and is 4 numbers
+        pass
 
     return render_template("request.html", applianceInfo=entry)
 
