d5b457d0a2b8438c82b8fd414f0e6de031fd7c7e
AnHaii <ah6253@nyu.edu>
2024-10-11 23:21:42 -0400

    add env property

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-import os
+from dotenv import load_dotenv
 import datetime
 from flask import Flask, render_template, request, redirect, url_for
 import pymongo
 from bson.objectid import ObjectId
-from dotenv import load_dotenv
+import os
 from pymongo.mongo_client import MongoClient
+load_dotenv()
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+uri = "mongodb+srv://thisapp:<db_password>@thisapp.pgts3.mongodb.net/?retryWrites=true&w=majority&appName=thisapp"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
