4ac74d929852c6e25d255f0b8d6ca5f46d4dc9f4
mkphung29 <mkp6075@nyu.edu>
2024-10-12 02:09:54 -0400

    Set up mongodb database connection

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
 from pymongo import MongoClient, server_api
+from dotenv import load_dotenv
+import os
+
+# load environment variables from .env vile
+load_dotenv()
+
 app = Flask(__name__, static_url_path="", static_folder="static", template_folder="templates")
+
+# set up mongodb database connection
+mongo_host = os.getenv("MONGO_HOSt")
+db_name = os.getenv("MONGO_DBNAME")
+
+client = MongoClient(mongo_host, server_api=server_api.ServerApi('1'))
+
+db = client[db_name]
+requests_collection = db.requests
 
 reports = ["Vending machine", "Water fountain", "Door hinge"]
 
