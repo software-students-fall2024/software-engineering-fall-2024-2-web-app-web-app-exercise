735b86a671f443076907e18c6a86759172a5711b
xc528 <xc2344@nyu.edu>
2024-10-12 22:54:19 -0400

    trim redundant space for manual input in add and edit and search

--- app.py
+++ app.py
@@ -183,8 +183,8 @@ def create_app():
                 flash("Invalid date format. Please enter a valid date in YYYY/MM/DD format.")
                 doc = {
                     "user_id": current_user.id,
-                    "job_title": job_title,
-                    "company": company,
+                    "job_title": " ".join(job_title.split()).strip(),#job_title,
+                    "company": " ".join(company.split()).strip(),
                     "location": location,
                     "link": link,
                     "stage": stage,
@@ -196,8 +196,8 @@ def create_app():
         
         doc = {
             "user_id": current_user.id,
-            "job_title": job_title,
-            "company": company,
+            "job_title": " ".join(job_title.split()).strip(),
+            "company": " ".join(company.split()).strip(),
             "location": location,
             "link": link,
             "stage": stage,
@@ -297,9 +297,9 @@ def create_app():
         time = request.form["time"]
         
         doc = {
-            "job_title": job_title,
-            "company": company,
-            "location": location,
+            "job_title": " ".join(job_title.split()).strip(),
+            "company": " ".join(company.split()).strip(),
+            "location": location,#" ".join(job_title.split()).strip(),
             "link": link,
             "stage": stage,
             "time": time
@@ -354,9 +354,9 @@ def create_app():
         search_criteria = {}
         search_criteria["user_id"] = current_user.id
         if job_title and job_title.strip():  # Check if job is not empty
-            search_criteria['job_title'] = {"$regex": f"^{job_title}$", "$options": "i"}#job_title
+            search_criteria['job_title'] = {"$regex": f"^{" ".join(job_title.split()).strip()}$", "$options": "i"}#job_title
         if company_name and company_name.strip():  # Check if company is not empty
-            search_criteria['company'] = {"$regex": f"^{company_name}$", "$options": "i"}#company_name
+            search_criteria['company'] = {"$regex": f"^{" ".join(company_name.split()).strip()}$", "$options": "i"}#company_name
         if location_name and location_name.strip():  # Check if location is not empty
             search_criteria['location'] = location_name
         if stage and stage.strip():  # Check if stage is not empty
