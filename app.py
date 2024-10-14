16049fa3fbadd1eb3b7118b90b2e4be5f12f62e6
cao-exe <tc3661@nyu.edu>
2024-10-13 22:43:38 -0400

    added documentation and removed a not implemented route

--- app.py
+++ app.py
@@ -8,35 +8,20 @@ logged_in = False
 projects = None
 
 
-################# Sample Data ################# 
-user_doc = {
-    "username": "admin",   # make usernames unique?, so that it can be searched with find_one()
-    "password": "admin",       # or add another unique user id 
-    "project_id": ["x43gbf34", "a87gkf39"],
-    "access_level": "admin",  # or "user"
-    "created_at": datetime.datetime.utcnow() # the date time now
-}
+############## Database Organization ###############
 
-project_doc = {
-    "project_name": "Project 1",
-    "project_id": "x43gbf34",     # unique id, can be searched with find_one()
-    "tasks": ["task1", "task2"],
-    "team members": ["user1", "user2"],  # all unique usernames of team members
-    "created_at": datetime.datetime.utcnow() # the date time now
-}
+''' Project Document Structure
+{"_id": ObjectId('________'),
+"projectName": "projectName",
+"managers": ["manager1", "manager2"],
+"members": ["member1", "member2"],
+"tasks": [{"taskName": "xxx", "date", "yyy", "status": "not-complete"}, ...]
 
-task_doc = {
-    "task_name": "task1",
-    "task_id": "hc619c71",     # unique id, can be searched with find_one()
-    "task_description": "This is task 1",
-    "task_status": "completed",
-    "task_members":["user1", "user2"],
-    "task_deadline": "2022-12-31",
-    "created_at": datetime.datetime.utcnow() # the date time now
-}
-###############################################
+'''
+#####################################################
 
 def create_app():
+    # APP SETUP
     app = Flask(__name__)
     app.secret_key = "KEY"
 
@@ -53,11 +38,13 @@ def create_app():
         print(e)
 
 
-
+    ############# ROUTES #############
+    # base route redirects to login
     @app.route("/")
     def base():
         return redirect(url_for('login'))
 
+    # main route displays home screen with all projects
     @app.route("/main")
     def home():
         global logged_in  
@@ -73,6 +60,10 @@ def create_app():
 
         return render_template("main.html", docs=projects, docs2=projects)
 
+    # route to login page
+    # if POST, check if username and password match
+    # if match, redirect to main
+    # if not, return to login with error message
     @app.route("/login", methods=['GET', 'POST'])
     def login():
         if request.method == 'POST':
@@ -97,6 +88,9 @@ def create_app():
         
         return render_template("login.html")
     
+    # route to register page
+    # if POST, check if username is taken
+    # if not, add to database and redirect to login
     @app.route("/register", methods=['GET', 'POST'])
     def register():
         if request.method == 'POST':
@@ -110,18 +104,6 @@ def create_app():
                 return render_template("registration.html", err="Username taken, please try again.")
         return render_template("registration.html")
     
-
-    @app.route("/logout")
-    def logout():
-        global logged_in  
-
-        # not logged in yet, return to login
-        if (logged_in == False):
-            return redirect(url_for('login'))
-        logout_user()
-        flash("Logged out successfully.", "info")
-        return redirect(url_for('login'))
-
     return app 
 
 if __name__ == "__main__":
