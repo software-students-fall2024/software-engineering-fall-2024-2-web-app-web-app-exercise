84084dc1d5686786d0658f510c3d2352a44f71c7
brshood <133962779+brshood@users.noreply.github.com>
2024-10-13 22:47:20 -0400

    Fixed .env and URI issue

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ def create_app():
     # Load environment variables from .env file
     load_dotenv()
 
-    # connection_string = os.getenv("MONGO_URI")  # Corrected to use "MONGO_URI" instead of MONGO_URI
+    connection_string = os.getenv("MONGO_URI")  # Corrected to use "MONGO_URI" instead of MONGO_URI
 
-    # if connection_string is None:
-    #     raise ValueError("MONGO_URI environment variable not found!")
+    if connection_string is None:
+        raise ValueError("MONGO_URI environment variable not found!")
 
     # # Create a MongoDB client with the correct connection string
-    # client = MongoClient(connection_string)
-    # db = client["test_db"]
-    # collection = db["test_collection"]
-    # sample_data = {"name": "John Doe", "age": 30, "city": "New York"}
-    # collection.insert_one(sample_data)
+    client = MongoClient(connection_string)
+    db = client["test_db"]
+    collection = db["test_collection"]
+    sample_data = {"name": "John Doe", "age": 30, "city": "New York"}
+    collection.insert_one(sample_data)
 
-    # print("Document inserted successfully!")
+    print("Document inserted successfully!")
     
     @app.route("/")
     def login():
