156ee70bca249b4095b234068770d4524f2cde65
dm6288 <dm6288@nyu.edu>
2024-10-15 02:35:42 -0400

    Added login error message

--- src/routes/login.py
+++ src/routes/login.py
@@ -9,15 +9,16 @@ from src.models.user import User
 from src.app import get_db
 from flask_login import login_required
 
-@routes.route("/",methods=['GET','POST'])
-@routes.route("/login",methods=['GET','POST'])
+
+@routes.route("/", methods=['GET', 'POST'])
+@routes.route("/login", methods=['GET', 'POST'])
 def login():
     users = get_db().users
     if flask.request.method == 'GET':
         return flask.render_template('login.html')
     email = flask.request.form['email']
     password = flask.request.form['password']
-    result = users.find_one({"email":email, "password":password})
+    result = users.find_one({"email": email, "password": password})
     if result:
         user = User()
         user.id = email
@@ -25,7 +26,9 @@ def login():
         flask_login.login_user(user)
         return flask.redirect(flask.url_for('routes.protected'))
 
-    return 'Bad login'
+    flask.flash('Invalid email or password. Please try again.', 'error')
+    return flask.redirect(flask.url_for('routes.login'))
+
 
 @routes.route('/protected')
 @flask_login.login_required
