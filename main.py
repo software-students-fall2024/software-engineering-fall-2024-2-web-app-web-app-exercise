95ecf42526c39757e3fe5574ec3a091fd0d084c5
drj8812 <drj8812@nyu.edu>
2024-10-15 00:18:52 -0400

    newAppliances.html and add_app() function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient, server_api
 import datetime
 from bson.objectid import ObjectId
@@ -18,6 +18,9 @@ client = MongoClient(mongo_host, server_api=server_api.ServerApi('1'))
 
 db = client[db_name]
 requests_collection = db.requests
+#collections for the appliances?
+building_collection = db.bldgs
+appliance_collection = db.appliances
 
 reports = ["Vending machine", "Water fountain", "Door hinge"]
 
@@ -33,6 +36,42 @@ def list():
 @app.route("/request/<requestID>")
 def makeRequest(requestID=None):
     return render_template("request.html", requestID=requestID)
+
+@app.route("/newAppliance", methods=["POST"])
+def add_app():
+    """
+    Route for POST requests to new appliances page.
+    Accepts form submission data and checks if the appliance exists.
+    If it does, asks if user wants to update data. If not, creates 
+    a new appliance with given data.
+    """
+
+    bname = request.form["bname"]
+    floor = request.form["floor"]
+    appName = request.form["appName"]
+    code = request.form["code"]
+
+    found: int = db.appliances.find({
+        #ignore code for now, just focus on name and appliance
+        "building":bname, 
+        "floor":floor,
+        "applianceName" : appName
+    }).count()
+
+    if found == 0:
+        #check code is not in use
+        doc = {
+            "code":code,
+            "building":bname,
+            "floor":floor,
+            "applianceName" : appName
+        }
+        db.appliances.insert_one(doc)
+        return redirect(url_for("index"))
+
+    else:
+        return redirect(url_for("index"))
+        #appliance exists, ask if they want to update code?
 
 
 if __name__ == "__main__":
