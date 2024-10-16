b7ab131381258914d272b130478120891ef8bd29
dm6288 <dm6288@nyu.edu>
2024-10-16 00:26:24 -0400

    Fixed errors in sign up and search screen

--- src/routes/signup.py
+++ src/routes/signup.py
@@ -7,24 +7,24 @@ import flask_login
 from src.app import get_db
 from src.models.user import user_loader, request_loader, User, find_user, create_user
 
+
 @routes.route("/sign-up", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
         email = request.form.get("email")
+        confirm_password = request.form.get("confirm-password")
         if not (username and password and email):
             return render_template("sign-up.html", message="All fields are required.")
 
         if find_user(email):
             return render_template("sign-up.html", message="This email is already associated with an account!")
-        
+
+        if password != confirm_password:
+            return render_template("sign-up.html", message="Passwords do not match.")
+
         create_user(email, username, password)
 
         return redirect("login")
     return render_template("sign-up.html")
-
-
-
-
-
