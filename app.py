1e9aff9a67f4d4d00e3e55290cb3e9159c03100f
Julie-Chen <juliechen623@outlook.com>
2024-10-18 03:10:11 -0400

    Basic page routing

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 
-load_dotenv() 
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('home.html')
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
