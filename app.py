5c8b89b967283d98718edef3b005705c8b5ee98c
Julie-Chen <juliechen623@outlook.com>
2024-10-18 03:56:57 -0400

    Added Post Feature with MongoDB

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+import os
 
 load_dotenv()
 
 app = Flask(__name__)
+MONGO_URI = os.getenv('MONGO_URI')
+client = MongoClient(MONGO_URI)
+
+db = client["diary"]
+entries = db["entries"]
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    all_entries = entries.find() 
+    return render_template('home.html', entries=all_entries)
 
-@app.route('/add')
+@app.route('/add', methods=['GET','POST'])
 def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        
+        entries.insert_one({'title': title, 'content': content})
+        return redirect(url_for('home'))
     return render_template('add.html')
 
 if __name__ == '__main__':
