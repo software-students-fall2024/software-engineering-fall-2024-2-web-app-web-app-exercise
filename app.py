9f4beffc8f1cdc6323a2c1f816dffaf564ac1f67
anerivs <aneri.shah@nyu.edu>
2024-10-18 15:40:02 -0400

    added edit functinoality for diary entries

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 import os
+from bson import ObjectId #to handle mongodb's objectid type
 
 load_dotenv()
 
@@ -26,6 +27,17 @@ def add():
         entries.insert_one({'title': title, 'content': content})
         return redirect(url_for('home'))
     return render_template('add.html')
+
+@app.route('/edit/<string:entry_id>', methods=['GET','POST'])
+def edit(entry_id):
+    if request.method == 'POST':
+        new_title = request.form['title']
+        new_content = request.form['content']
+        entries.update_one({'_id': ObjectId(entry_id)}, {'$set': {'title': new_title, 'content': new_content}})
+        return redirect(url_for('home'))
+    entry = entries.find_one({'_id': ObjectId(entry_id)})
+    return render_template('edit.html', entry=entry)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
